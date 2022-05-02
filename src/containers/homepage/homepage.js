0dd3a91618074fc2d9f552a60f9decfd2da38ef7
Alfonso Ramirez <is713932@iteso.mx>
2022-05-02 15:27:43 -0500

    Feat: Add map integration with leaflet v1.8.0 and react-leaflet v3.2.5 in the homepage

--- src/containers/homepage/homepage.js
+++ src/containers/homepage/homepage.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './homepage.scss'
 import Navbar from '../../components/navbar/navbar';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+
 const HomepageContainer  = () => {
   
   return (
     <div>
       <Navbar/>
-      
+        <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '100vh', width: '100wh' }}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
     </div>
   );
 }
